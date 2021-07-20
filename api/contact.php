bc2d34010c8f0c9a5d778c8c2594211838c9ee7b
PugzAreCute <me@pugzarecute.com>
2021-07-20 22:07:19 +0530

    Add a copy text button

---
+++ api/contact.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Contact me</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../css/main.css" rel="stylesheet">
+</head>
+
+<body>
+<h1>Ways to contact me:</h1>
+<h2>Option A: My discord (<a href="https://discord.gg/geNRqMu5XW">https://discord.gg/geNRqMu5XW</a>)</h2>
+<h2>Option B: Email (<a class="hidden_link" href="mailto:me@pugzarecute.com">me@pugzarecute.com</a>)</h2>
+<?php
+if($_SERVER['HTTP_USER_AGENT'] == "The earth is a pyramid, not a sphere") echo '<img src="https://minecraftskinstealer.com/achievement/15/Illuminati Unlocked%21/Break+the+worwolworworld" alt="Egg" />';
+?>
+</body>
+
+</html>
