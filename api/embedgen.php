125efdb61ce0c5f01a6e285e1799fe9850f63bc8
PugzAreCute <me@pugzarecute.com>
2021-07-20 11:37:06 +0530

    Make embedgen.php not so blank

--- api/embedgen.php
+++ api/embedgen.php
@@ -1,5 +1,6 @@
 <?php
     echo "<head>";
+    echo "<link href='../css/main.css' rel='stylesheet'>";
     echo "<meta name=\"description\" content=\"".$_GET["description"]."\">";
     echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
     echo "<meta property=\"og:title\" content=\"".$_GET["title"]."\">";
@@ -9,4 +10,5 @@
     echo "<meta property=\"og:image\" content=\"".$_GET["image"]."\">";
     echo "<meta name=\"theme-color\" content=\"#".$_GET["color"]."\">";
     echo "</head>";
+    echo "<body><h1> Paste "."https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']." In your favourate chat app that supports OG to preview this embed</h1></body>";
 
