a3ce916d61b9724f35a0f4327ae1f73bf592b4aa
PugzAreCute <me@pugzarecute.com>
2021-07-20 12:46:17 +0530

    Try to fix video

--- api/embedgen.php
+++ api/embedgen.php
@@ -4,12 +4,15 @@
     echo "<meta name=\"description\" content=\"".$_GET["description"]."\">";
     echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
     echo "<meta property=\"og:title\" content=\"".$_GET["title"]."\">";
-    echo "<meta property=\"og:type\" content=\"text/strings\">";
+    if($_GET['video'] == "") echo "<meta property=\"og:type\" content=\"text/strings\">";
+    else echo "<meta property=\"og:type\" content=\"video.other\">";
     echo "<meta property=\"og:url\" content=\"".$_GET["url"]."\">";
     echo "<meta property=\"og:description\" content=\"".$_GET["description"]."\">";
     echo "<meta property=\"og:image\" content=\"".$_GET["image"]."\">";
     echo "<meta name=\"theme-color\" content=\"#".$_GET["color"]."\">";
     echo "<meta name=\"og:video\" content=\"".$_GET["video"]."\">";
+    echo "<meta name=\"og:video:secure_url\" content=\"".$_GET["video"]."\">";
+    echo "<meta name=\"og:video:type\" content=\"video/mp4\">";
     echo "</head>";
     echo "<body><h1> Paste "."https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']." In your favourate chat app that supports OG to preview this embed</h1></body>";
 
