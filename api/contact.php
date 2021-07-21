c3bb0f97ec3693b7823228e987d6e3db79f284fa
PugzAreCute <me@pugzarecute.com>
2021-07-21 11:00:17 +0530

    Yay users can finally find the easteregg

--- api/contact.php
+++ api/contact.php
@@ -12,6 +12,8 @@
 <h1>Ways to contact me:</h1>
 <h2>Option A: My discord (<a href="https://discord.gg/geNRqMu5XW">https://discord.gg/geNRqMu5XW</a>)</h2>
 <h2>Option B: Email (<a class="hidden_link" href="mailto:me@pugzarecute.com">me@pugzarecute.com</a>)</h2>
+
+<h2>Easter egg: Try setting your UserAgent as The earth is a pyramid, not a sphere</h2>
 <?php
 if($_SERVER['HTTP_USER_AGENT'] == "The earth is a pyramid, not a sphere") echo '<img src="https://minecraftskinstealer.com/achievement/15/Illuminati Unlocked%21/Break+the+worwolworworld" alt="Egg" />';
 ?>
