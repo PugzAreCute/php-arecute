d8bb98ac51c610e051f8987c8f96009bafcafa04
PugzAreCute <me@pugzarecute.com>
2021-07-22 14:21:25 +0530

    Test ASN

--- api/iputils.php
+++ api/iputils.php
@@ -6,7 +6,17 @@
     <link rel="stylesheet" href="../css/main.css">
     </head>
 <body>
-<h1>WIP, check back later</h1>
+<ul id="nav">
+    <li><a href="/">Home</a></li>
+    <li><a href="/contact">Contact</a></li>
+    <li><a href="embed.html">Discord embed generator</a></li>
+    <li><a href="/iputils">IP Utilities</a></li>
+</ul>
+<?php
+echo "<h1> IP Address: ".$_SERVER['REMOTE_ADDR']."</h1>";
+echo "<h1> ASN: ".geoip_asnum_by_name($_SERVER["REMOTE_ADDR"])."</h1>";
+?>
+
 <a href="https://vercel.com/?utm_source=pugsmods&utm_campaign=oss"><img src="../powered-by-vercel.svg"></a>
 </body>
 </html>
