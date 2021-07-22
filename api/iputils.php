72f44ee34006ce4d47a5a85a1aa5d1ef4932c7eb
PugzAreCute <me@pugzarecute.com>
2021-07-22 14:44:08 +0530

    remov phpinfo

--- api/iputils.php
+++ api/iputils.php
@@ -13,9 +13,9 @@
     <li><a href="/iputils">IP Utilities</a></li>
 </ul>
 <?php
-phpinfo();
+//phpinfo();
 echo "<h1> IP Address: ".$_SERVER['REMOTE_ADDR']."</h1>";
-echo "<h1> ASN: ".geoip_asnum_by_name($_SERVER["REMOTE_ADDR"])."</h1>";
+//echo "<h1> ASN: ".geoip_asnum_by_name($_SERVER["REMOTE_ADDR"])."</h1>";
 ?>
 
 <a href="https://vercel.com/?utm_source=pugsmods&utm_campaign=oss"><img src="../powered-by-vercel.svg"></a>
