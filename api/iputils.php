60a0f6b210043d92d972e9144abd6261e853f7c7
PugzAreCute <me@pugzarecute.com>
2021-07-22 14:37:41 +0530

    phpinfo

--- api/iputils.php
+++ api/iputils.php
@@ -13,6 +13,7 @@
     <li><a href="/iputils">IP Utilities</a></li>
 </ul>
 <?php
+phpinfo();
 echo "<h1> IP Address: ".$_SERVER['REMOTE_ADDR']."</h1>";
 echo "<h1> ASN: ".geoip_asnum_by_name($_SERVER["REMOTE_ADDR"])."</h1>";
 ?>
