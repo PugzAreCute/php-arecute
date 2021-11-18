79ca83996bf48c87eec4875121327ab0588aab16
PugzAreCute <me@pugzarecute.com>
2021-11-18 21:05:58 +0530

    Stuff

--- api/ci.php
+++ api/ci.php
@@ -12,18 +12,18 @@ $curl = curl_init();
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_URL, "https://argon.pugzarecute.com/guestAuth/app/rest/builds?projectLocator=name:WoneWay");
 curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json'));
-$parsedJSON_buildList = json_decode (curl_exec($curl));
+$parsedJSON_buildList = json_decode(curl_exec($curl));
 //print_r( $parsedJSON->build);
-foreach ($parsedJSON_buildList->build as $curr){
+foreach ($parsedJSON_buildList->build as $curr) {
     $buildcurl = curl_init();
     curl_setopt($buildcurl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($buildcurl, CURLOPT_HTTPHEADER, array('Accept: application/json'));
-    curl_setopt($buildcurl, CURLOPT_URL, "https://argon.pugzarecute.com/guestAuth/app/rest/builds/id:".$curr->number."/artifacts/children");
+    curl_setopt($buildcurl, CURLOPT_URL, "https://argon.pugzarecute.com/guestAuth/app/rest/builds/id:" . $curr->number . "/artifacts/children");
     $buildArtifacts = json_decode(curl_exec($buildcurl));
-    echo "<div><a href=\"https://argon.pugzarecute.com/".$buildArtifacts->file[0]->href."\"><button class=\"ci_build\">
-    <h3>Build #".$curr->number."</h3>
-    <h4>Build status: ".$curr->status."</h4>
-    <p>Artifact Info:<br>Size:".$buildArtifacts->file[0]->size."<br>Build Time: ".$buildArtifacts->file[0]->modificationTime."</p>
+    echo "<div><a href=\"https://argon.pugzarecute.com/" . $buildArtifacts->file[0]->href . "\"><button class=\"ci_build\">
+    <h3>Build #" . $curr->number . "</h3>
+    <h4>Build status: " . $curr->status . "</h4>
+    <p>Artifact Info:<br>Size:" . $buildArtifacts->file[0]->size . "<br>Build Time: " . $buildArtifacts->file[0]->modificationTime . "</p>
 </button></a></div>";
     print_r($buildArtifacts->file);
 }
