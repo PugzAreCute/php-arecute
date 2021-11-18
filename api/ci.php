9020ad659d7d9e240ee5159ebf6e63392c1b4426
PugzAreCute <me@pugzarecute.com>
2021-11-18 16:55:24 +0530

    Now prints!

--- api/ci.php
+++ api/ci.php
@@ -12,12 +12,15 @@ curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_URL, "");
 curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json'));
 $parsedJSON = json_decode (curl_exec($curl));
-print_r( $parsedJSON->build[0]->status)
+//print_r( $parsedJSON->build);
+foreach ($parsedJSON->build as $curr){
+    echo "<div class=\"ci_build\">
+    <h3>Build #".$curr->number."</h3>
+    <h4>Build status: ".$curr->status."</h4>
+</div>";
+}
 ?>
-<div class="ci_build">
-    <h3>Build #{Build_Number}</h3>
-    <h4>Build status: </h4>
-</div>
+
 <a href="https://vercel.com/?utm_source=pugsmods&utm_campaign=oss"><img alt="Powered By Vercel!"
                                                                         src="../img/Vercel.svg"></a>
 </body>
