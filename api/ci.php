49b3b585c2056fafc282545a7f3f7576ae5bbf26
PugzAreCute <me@pugzarecute.com>
2021-11-19 06:44:11 +0530

    Fix!

--- api/ci.php
+++ api/ci.php
@@ -8,6 +8,7 @@
 <body>
 <h1>Click on a build card to download it.</h1>
 <?php
+error_reporting(0);
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_URL, "https://argon.pugzarecute.com/guestAuth/app/rest/builds?projectLocator=name:WoneWay");
@@ -20,7 +21,7 @@ foreach ($parsedJSON_buildList->build as $curr) {
     curl_setopt($buildcurl, CURLOPT_HTTPHEADER, array('Accept: application/json'));
     curl_setopt($buildcurl, CURLOPT_URL, "https://argon.pugzarecute.com/guestAuth/app/rest/builds/id:" . $curr->number . "/artifacts/children");
     $buildArtifacts = json_decode(curl_exec($buildcurl));
-    echo "<div><a href=\"https://argon.pugzarecute.com/" . $buildArtifacts->file[0]->href . "\"><button class=\"ci_build\">
+    echo "<div><a href=\"https://argon.pugzarecute.com" . $buildArtifacts->file[0]->href . "\"><button class=\"ci_build\">
     <h3>Build #" . $curr->number . "</h3>
     <h4>Build status: " . $curr->status . "</h4>
     <p>Artifact Info:<br>Size:" . $buildArtifacts->file[0]->size . "<br>Build Time: " . $buildArtifacts->file[0]->modificationTime . "</p>
