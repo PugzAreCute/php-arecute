ca1bd3d6fd4312e0da02473b054c24b6600c47e9
PugzAreCute <me@pugzarecute.com>
2021-12-31 17:39:52 +0530

    Fix Failed builds still linking to an artifact.

--- api/ci.php
+++ api/ci.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Pugsmods CI</title>
+    <title>PugsMods CI</title>
     <link href="../css/main.css" rel="stylesheet">
 
     <meta content="Pugsmods CI" property="og:title">
@@ -35,12 +35,22 @@ if ($shouldContinueCurling == true) {
         curl_setopt($buildcurl, CURLOPT_HTTPHEADER, array('Accept: application/json'));
         curl_setopt($buildcurl, CURLOPT_URL, "https://argon.pugzarecute.com/ci/guestAuth/app/rest/builds/id:" . $curr->id . "/artifacts/children");
         $buildArtifacts = json_decode(curl_exec($buildcurl));
-        echo "<div><a href=\"https://argon.pugzarecute.com/ci" . $buildArtifacts->file[0]->content->href . "\"><button class=\"ci_build\">
+        echo "<div>";
+         if ($curr->status == "SUCCESS"){
+            echo "<a href=\"https://argon.pugzarecute.com/ci" . $buildArtifacts->file[0]->content->href . "\">";
+         }
+        echo "<button class=\"ci_build\">
     <h3>Build #" . $curr->number . "</h3>
     <h4>Build status: " . $curr->status . "</h4>
-    <p>Artifact Info:<br>File name: " . $buildArtifacts->file[0]->name . "<br>Size:" . $buildArtifacts->file[0]->size . " bytes<br>Build Time: " . $buildArtifacts->file[0]->modificationTime . "</p>
-</button></a></div>";
-
+    ";
+    if ($curr->status == "SUCCESS"){
+        echo "<p>Artifact Info:<br>File name: " . $buildArtifacts->file[0]->name . "<br>Size:" . $buildArtifacts->file[0]->size . " bytes<br>Build Time: " . $buildArtifacts->file[0]->modificationTime . "</p>";
+    }
+    echo "</button>";
+    if ($curr->status == "SUCCESS"){
+        echo "</a>";
+    }
+    echo "</div>";
         curl_close($buildcurl);
     }
 } else {
