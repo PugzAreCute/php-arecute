424797103283f1b9fdfd8fc6d1526c91c0a47b7c
PugzAreCute <me@pugzarecute.com>
2022-01-17 12:17:22 +0530

    Does this work?

--- api/headertoe.php
+++ api/headertoe.php
@@ -1,4 +1,7 @@
 <?php
 if ($_SERVER['HTTP_X_START_SESSION'] == "TRUE"){
+    $gamePath=tempnam("/tmp","game-");
+    echo $gamePath;
     echo "200 Request Accepted. No further actions needed";
+    echo "200 Login credentials sent through response headers. Please authorize in a few seconds to make sure that your game has been created.";
 }
