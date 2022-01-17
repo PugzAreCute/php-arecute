b99db2c43ab8a6fc3e7910c854e1ef32fd9b526f
PugzAreCute <me@pugzarecute.com>
2022-01-17 13:43:45 +0530

    Yes

--- api/headertoe.php
+++ api/headertoe.php
@@ -2,11 +2,14 @@
 if ($_SERVER['HTTP_X_START_SESSION'] == "TRUE"){
     $gamePath=substr(tempnam("/tmp","game-"),5);
     $fileGame = fopen("/tmp/".$gamePath,"w");
+    fclose($fileGame);
+    $fileGame = fopen("/tmp/".$gamePath,"r+");
     $randKey = substr(tempnam("/tmp",""),5);
     fwrite($fileGame,$randKey."\n");
     header("x-security-key: ".$randKey);
     header("x-game-code: ". $gamePath);
     echo "200 Request Accepted. No further actions needed";
     echo "200 Login credentials sent through response headers. Please authorize in a few seconds to make sure that your game has been created.";
-    echo fread($fileGame, filesize($fileGame));
+    echo fread($fileGame,filesize("/tmp/".$gamePath));
+    fclose($fileGame);
 }
