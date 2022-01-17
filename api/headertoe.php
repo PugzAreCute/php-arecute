c96cbb7d0270d5803ee633cc1d2162ecc6f2e4b8
PugzAreCute <me@pugzarecute.com>
2022-01-17 12:44:27 +0530

    Stuff

--- api/headertoe.php
+++ api/headertoe.php
@@ -1,7 +1,13 @@
 <?php
 if ($_SERVER['HTTP_X_START_SESSION'] == "TRUE"){
-    $gamePath=tempnam("/tmp","game-");
-    echo $gamePath;
+    $gamePath=substr(tempnam("/tmp","game-"),5);
+    $fileGame = fopen("/tmp/".$gamePath,"w");
+    $randKey = openssl_random_pseudo_bytes(100);
+    fwrite($fileGame,$randKey."\n");
+    header("x-security-key",$randKey);
+    header("x-game-code", $gamePath);
     echo "200 Request Accepted. No further actions needed";
     echo "200 Login credentials sent through response headers. Please authorize in a few seconds to make sure that your game has been created.";
+    echo fread($fileGame,filesize("/tmp/".$gamePath));
+    fclose($fileGame);
 }
