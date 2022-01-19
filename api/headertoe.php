2d4eafaf9d438ede21aa5e08c8d8d15ed719e3b4
PugzAreCute <me@pugzarecute.com>
2022-01-19 07:35:05 +0530

    Hmm?

--- api/headertoe.php
+++ api/headertoe.php
@@ -1,16 +1,16 @@
 <?php
 if ($_SERVER['HTTP_X_START_SESSION'] == "TRUE"){
-    $gamePath=substr(tempnam("/tmp","game-"),5);
-    $fileGame = fopen("/tmp/".$gamePath,"w");
+    $gamePath=substr(tempnam("/tmp","game-"),5).".txt";
+    $fileGame = fopen("/tmp/".$gamePath.".txt","w+");
     fwrite($fileGame,"Test===");
     fclose($fileGame);
-    $fileGame = fopen("/tmp/".$gamePath,"r+");
+    $fileGame = fopen("/tmp/".$gamePath.".txt","r+");
     $randKey = substr(tempnam("/tmp",""),5);
     fwrite($fileGame,$randKey."\n");
     header("x-security-key: ".$randKey);
     header("x-game-code: ". $gamePath);
     echo "200 Request Accepted. No further actions needed";
     echo "200 Login credentials sent through response headers. Please authorize in a few seconds to make sure that your game has been created.";
-    echo fread($fileGame,filesize("/tmp/".$gamePath));
+    echo fread($fileGame,filesize("/tmp/".$gamePath.".txt"));
     fclose($fileGame);
 }
